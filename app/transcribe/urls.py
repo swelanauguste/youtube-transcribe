544bf89a8a587a22850af1bfc5c7b6555134d70b
swelanauguste <swelanauguste@gmail.com>
2023-05-05 00:26:57 +0000

    urls

--- app/transcribe/urls.py
+++ app/transcribe/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import TranscribeCreateView, TranscribeDetailView, TranscribeListView
 
 urlpatterns = [
-    path("list/", TranscribeListView.as_view(), name="list"),
+    path("transcripts/", TranscribeListView.as_view(), name="list"),
     path(
         "detail/<slug:slug>/", TranscribeDetailView.as_view(), name="detail"
     ),
